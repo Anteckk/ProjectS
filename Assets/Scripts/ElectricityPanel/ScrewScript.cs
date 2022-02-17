9e7354b3072442961adfb2f109f4a5145d60836b
Anteckk <antoine.varniern@gmail.com>
2022-02-17 12:08:25 +0100

    Add Highlight when mouse over screw

--- Assets/Scripts/ElectricityPanel/ScrewScript.cs
+++ Assets/Scripts/ElectricityPanel/ScrewScript.cs
@@ -7,11 +7,17 @@ public class ScrewScript : MonoBehaviour
     public DOTweenAnimation moveAnim;
     public DOTweenAnimation rotateAnim;
     public GameObject parent;
+    public Material highligthMaterial;
+    private Material baseMaterial;
+    private MeshRenderer mesh;
+    
     private GameManager _gameManager;
 
     private void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
+        mesh = GetComponent<MeshRenderer>();
+        baseMaterial = mesh.material;
     }
 
     public void DestroyThis()
@@ -27,5 +33,18 @@ public class ScrewScript : MonoBehaviour
             moveAnim.DOPlay();
             rotateAnim.DOPlay();
         }
+    }
+
+    public void OnMouseEnter()
+    {
+        if (_gameManager.GetPlayerInventory().GetEquippedItem().TypeOfItem == Item.ItemType.Screwdriver)
+        {
+            mesh.material = highligthMaterial;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        mesh.material = baseMaterial;
     }
 }
