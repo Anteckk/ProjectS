71af1e42f5dc2abfa67a25593f0b31ccbbe2bbb9
Elvyn Wakeford <elvynwakeford@gmail.com>
2022-03-03 19:32:03 +0100

    added post proc addon, walls now diasapere with cam orientation

--- Assets/Scripts/Player/CameraBehaviour.cs
+++ Assets/Scripts/Player/CameraBehaviour.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Cinemachine;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.Networking;
 using UnityEngine.Serialization;
 
 public class CameraBehaviour : MonoBehaviour
@@ -16,13 +21,26 @@ public class CameraBehaviour : MonoBehaviour
 
     public PlayerController player;
 
-    public Material transparentMaterial;
-    public Material originalMaterial;
-    
-    private GameObject wall;
-    
+    [FormerlySerializedAs("WallEmptyGameObject")]
+    public GameObject WallGameObject;
+
+
+    private List<GameObject> _walls = new List<GameObject>();
+
+
     RaycastHit hit;
 
+
+    private void Awake()
+    {
+        for (int i = 0; i < WallGameObject.transform.childCount; i++)
+        {
+            _walls.Add(WallGameObject.transform.GetChild(i).gameObject);
+        }
+
+        Debug.Log("Added walls to walls array. childCount : " + WallGameObject.transform.childCount
+                                                              + ". Walls length : " + _walls.Count);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -33,17 +51,13 @@ public class CameraBehaviour : MonoBehaviour
         player = GetComponentInParent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
 
     void OnCameraClick(InputValue prmInputValue)
     {
         if (prmInputValue.isPressed)
         {
             OrbitalTransposer.m_XAxis.m_MaxSpeed = 150;
-            InvokeRepeating(nameof(WallTransparency), 0f, 0.1f );
+            InvokeRepeating(nameof(WallTransparency), 0f, 0.1f);
         }
         else
         {
@@ -55,29 +69,56 @@ public class CameraBehaviour : MonoBehaviour
         }
     }
 
+
     public void WallTransparency()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        var transformCamera = camera.transform;
+        if (Physics.SphereCast(transformCamera.position, 1, transformCamera.forward, out hit))
         {
-            if (hit.collider.gameObject.CompareTag("Wall"))
-            {
-                
-                if (wall != hit.collider.gameObject)
+            if (hit.transform.gameObject.CompareTag("WallCenterTrigger"))
                 {
-                    if (wall)
-                    {
-                        wall.GetComponent<MeshRenderer>().material = originalMaterial;
-                    }
-                    wall = hit.collider.gameObject;
+                    SetWallState("WallCenter", false);
                 }
-                wall.GetComponent<MeshRenderer>().material = transparentMaterial;
-            }
             else
             {
-                if (wall)
-                {
-                    wall.GetComponent<MeshRenderer>().material = originalMaterial;
-                }
+                SetWallState("WallCenter", true);
+            }
+        }
+
+
+        Vector3 Direction = transform.forward;
+
+
+        if (Direction.x > 0)
+        {
+            SetWallState("WallNorth", false);
+            SetWallState("WallSouth", true);
+        }
+        else
+        {
+            SetWallState("WallNorth", true);
+            SetWallState("WallSouth", false);
+        }
+
+        if (Direction.z > 0)
+        {
+            SetWallState("WallWest", false);
+            SetWallState("WallEast", true);
+        }
+        else
+        {
+            SetWallState("WallWest", true);
+            SetWallState("WallEast", false);
+        }
+    }
+
+    public void SetWallState(String prmTag, bool prmState)
+    {
+        foreach (var wall in _walls)
+        {
+            if (wall.CompareTag(prmTag))
+            {
+                wall.SetActive(prmState);
             }
         }
     }
